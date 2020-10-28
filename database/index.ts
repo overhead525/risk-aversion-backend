a4f8f6e3579022f0897a3f812a6d88e71e144262
overhead525 <ritchelcousar@outlook.com>
2020-10-28 19:30:12 -0400

    cleaned up server routes for flawless server run

--- database/index.ts
+++ database/index.ts
@@ -4,64 +4,64 @@ require("dotenv").config();
 type CallBacksArr = Array<() => any | (() => Promise<any>)>;
 
 interface dbProps {
-    callbacks: CallBacksArr;
+  callbacks: CallBacksArr;
 }
 
 const exampleCallback = async () => {
-    console.log("connection has been made");
-    const collections = await mongoose.connection.db
-        .listCollections()
-        .toArray();
-    console.log(collections);
+  console.log("connection has been made");
+  const collections = await mongoose.connection.db.listCollections().toArray();
+  console.log(collections);
 };
 
 const dbObject: dbProps = {
-    callbacks: [exampleCallback],
+  callbacks: [exampleCallback],
 };
 
 export const db = (dbconfig: dbProps) => {
-    mongoose.connect(process.env["DATABASE_URL"], {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+  mongoose.connect(process.env["DATABASE_URL"], {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
-    mongoose.connection
-        .once("open", () => {
-            try {
-                const callbacks: CallBacksArr = dbconfig.callbacks;
-                callbacks.forEach((callback) => {
-                    callback();
-                });
-            } catch (error) {
-                return new Error(
-                    "There was a problem with one of your callbacks"
-                );
-            }
-        })
-        .on("error", (error) => {
-            console.log("error is", error);
+  mongoose.connection
+    .once("open", () => {
+      try {
+        const callbacks: CallBacksArr = dbconfig.callbacks;
+        callbacks.forEach((callback) => {
+          callback();
         });
+      } catch (error) {
+        return new Error("There was a problem with one of your callbacks");
+      }
+    })
+    .on("error", (error) => {
+      console.log("error is", error);
+    });
 };
 
 export const useDB = (cb: Function) => {
-    mongoose.connect(process.env["DATABASE_URL"], {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+  mongoose.connect(process.env["DATABASE_URL"], {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
-    mongoose.connection
-        .once("open", () => {
-            console.log(`database connection opened to MongoDB on port ${mongoose.connection.port}`);
-            try {
-                cb();
-            } catch (error) {
-                return error;
-            }
-        })
-        .on("error", (error) => {
-            console.error("error is", error);
-        })
-        .on("close", () => {
-            console.log(`database conneciton closed on port: ${mongoose.connection.port}`)
-        })
-}
+  mongoose.connection
+    .once("open", () => {
+      console.log(
+        `database connection opened to MongoDB on port ${mongoose.connection.port}`
+      );
+      try {
+        cb();
+      } catch (error) {
+        return error;
+      }
+    })
+    .on("error", (error) => {
+      console.error("error is", error);
+    })
+    .on("close", () => {
+      console.log(
+        `database conneciton closed on port: ${mongoose.connection.port}`
+      );
+    });
+};
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { config } from "dotenv";
 config();
 
 import * as express from "express";
+import * as mongoose from "mongoose";
 import * as morgan from "morgan";
 import { allowHeader } from "./auth/helpers";
 
@@ -10,9 +11,16 @@ import core from "./core";
 
 const app = express();
 
-app.use(express.json());
-app.use(morgan("dev"));
+mongoose
+  .connect(process.env["DATABASE_URL"], {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.use(express.json());
+    app.use(morgan("dev"));
 
-app.use("/graphql", allowHeader, core);
+    app.use("/graphql", allowHeader, core);
 
-app.listen(3000, () => console.log("server running on port 3000 🚀"));
+    app.listen(3000, () => console.log("server running on port 3000 🚀"));
+  });
